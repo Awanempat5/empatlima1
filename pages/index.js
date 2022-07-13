f6e830bef17f3feae7bbae53d8de5a15b8a5d610
Awanempat5 <109128510+Awanempat5@users.noreply.github.com>
2022-07-14 01:36:04 +0800

    update index.js

--- pages/index.js
+++ pages/index.js
@@ -150,6 +150,28 @@ const Home = () => (
               </Button>
             </Link>
           </ListItem>
+           <ListItem>
+            <Link href="#" target="_blank">
+              <Button
+                variant="ghost"
+                colorScheme="teal"
+                leftIcon={<IoLogoTwitter />}
+              >
+                @Awanempat5
+              </Button>
+            </Link>
+          </ListItem>
+ <ListItem>
+            <Link href="#" target="_blank">
+              <Button
+                variant="ghost"
+                colorScheme="teal"
+                leftIcon={<IoLogoInstagram />}
+              >
+                @Awanempat5
+              </Button>
+            </Link>
+          </ListItem>
         </List>
 
         <SimpleGrid columns={[1, 2, 2]} gap={6}>
